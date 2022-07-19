0eaa840aa4d7b1c17c3010f82ad85cb318899301
Sarah <sarahmahmood692@gmail.com>
2022-07-19 20:49:23 +0500

    final changes

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 import pytest
+from os.path import abspath
+import sys, os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+chromepath = os.path.join(os.path.sep, ROOT_DIR, 'drivers' + os.sep)
+
 
 
 @pytest.fixture()
 def setup():
-    # driver = webdriver.Chrome(executable_path='../drivers/chromedriver.exe')
-    driver = webdriver.Chrome(r"C:\Users\Saad\PycharmProjects\sauceDemo1\drivers\chromedriver.exe")
+    chromepath = abspath("C:\\Users\\Saad\\PycharmProjects\\sauceDemo1\\drivers\\chromedriver.exe")
+    driver = webdriver.Chrome(chromepath)
     # driver.get('https://www.saucedemo.com/')
     return driver
