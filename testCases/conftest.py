818003dfe3d9293fcf9cfcd7681aa4fb4667fc3c
Sarah <sarahmahmood692@gmail.com>
2022-07-19 20:54:30 +0500

    done

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -10,6 +10,7 @@ chromepath = os.path.join(os.path.sep, ROOT_DIR, 'drivers' + os.sep)
 
 @pytest.fixture()
 def setup():
+    # driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe")
     chromepath = abspath("C:\\Users\\Saad\\PycharmProjects\\sauceDemo1\\drivers\\chromedriver.exe")
     driver = webdriver.Chrome(chromepath)
     # driver.get('https://www.saucedemo.com/')
